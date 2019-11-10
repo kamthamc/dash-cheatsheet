d631bba88ddd1f9c388471efd746727d8db85eb0
Chaitanya Kumar Kamatham <kamthamc@users.noreply.github.com>
2019-11-09 22:28:48 -0800

    feat: added changelog and removed docset

--- .releaserc.js
+++ .releaserc.js
@@ -3,11 +3,19 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
     [
       '@semantic-release/github',
       {
         assets: [{ path: 'Git.docset.gz', label: 'Git Cheatsheet' }]
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version} [skip-ci]\n\n${nextRelease.notes}'
       }
     ]
   ]
